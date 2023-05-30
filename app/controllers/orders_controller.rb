6adb2e39ca5dbbc2267003db649001950e0a3115
Alphonse Bonnin <alphonse.bonnin@gmail.com>
2023-05-30 19:01:33 -0300

    added my-rentals and start orders model. Issue with order scope still though

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,7 @@
 class OrdersController < ApplicationController
+  def my_rentals
+    # rented_guitars = Order.where('start_date <= ? AND end_date >= ?', Date.today, Date.today)
+    @orders = policy_scope(Order)
+    # authorize @orders
+  end
 end
