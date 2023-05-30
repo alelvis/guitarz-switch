ea39ad32d679ba1d7926fad1fa03c4b26f06605c
thiago laredo <thiago@olatu.com.br>
2023-05-30 10:43:25 -0300

    criação do metodo

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -3,4 +3,9 @@ class GuitarsController < ApplicationController
   def index
     @guitars = Guitar.all
   end
+
+  def show
+    @guitar = Guitar.find(params[:id])
+    @guitars = Guitar.new
+  end
 end
