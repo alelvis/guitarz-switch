bfeefa29feef7abdeb4bf49b8f25c936251d7e9a
Júlia von der Heyde <juliavdheyde@icloud.com>
2023-06-01 14:26:18 -0300

    Filter Date implemented

--- app/controllers/guitars_controller.rb
+++ app/controllers/guitars_controller.rb
@@ -12,6 +12,11 @@ class GuitarsController < ApplicationController
     if params[:query].present?
       @guitars = @guitars.search_brand_and_city(params[:query])
     end
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+      @guitars =  @guitars.joins(:orders).where.not('start_date = ? AND end_date = ?', start_date, end_date)
+    end
   end
 
   def my_guitars
